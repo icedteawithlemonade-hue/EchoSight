475f7db5c6c1db4044bba88380b06d3630e269dd
Aditya Verma <trivani.d.verma@gmail.com>
2026-01-28 18:42:59 -0500

    Initial Commit

---
+++ EchoSight/EchoSightApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoSightApp.swift
+//  EchoSight
+//
+//  Created by Ram Verma on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct EchoSightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
